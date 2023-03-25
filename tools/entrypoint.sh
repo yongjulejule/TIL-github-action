fea0a0c05177d694a9ea41de0eb473171f234724
yongjulejule <lyjshow200@gmail.com>
2023-03-25 14:00:32 +0900

    :hammer: chore : github output test

--- tools/entrypoint.sh
+++ tools/entrypoint.sh
@@ -2,7 +2,8 @@
 
 echo "hello action with docker"
 
-echo "this is github output about-til='thisisTIL'" > ${GITHUB_OUTPUT:-/dev/stdout}
+echo $GITHUB_OUTPUT
+echo "about-til='thisisTIL'" > ${GITHUB_OUTPUT:-/dev/stdout}
 
 if [[ $1 =~ "generateREADME.sh" ]]; then
   set -- $@ -f README.md
