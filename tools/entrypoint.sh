9a9239a20b4b213e1e04566876cd6a855b3adbce
yongjulejule <lyjshow200@gmail.com>
2023-03-25 14:50:02 +0900

    :hammer: chore : fix typo

--- tools/entrypoint.sh
+++ tools/entrypoint.sh
@@ -7,7 +7,7 @@ cd $GITHUB_WORKSPACE
 git config --global --add safe.directory /github/workspace
 git config --local user.email "action@github.com"
 git config --local user.name "GitHub Action"
-git swirch -c readme-bot
+git switch -c readme-bot
 git add README.md
 git commit -m "Update README.md"
 git push origin readme-bot
